1cee1a095a1ecb26fc39c86e487c9673c79a8d44
tejhons <tejh.tech@gmail.com>
2026-08-31 20:57:40 -0400

    External variables init

---
+++ C/exercises/External_variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int max;
+char current_line[MAXLINE];
+char longest_line[MAXLINE];
+/* Notice initialized outside of main */
+
+int getline(void);
+void copy(void);
+
+int main() 
+{
+  /* longest input line specialized version */
+  
+  
+  return 0;
+}
