3f55af365d9d944ff777db725ee982f9d2c4f925
tejhons <tejh.tech@gmail.com>
2026-09-01 18:01:22 -0400

    starting gcio

--- C/exercises/External_variables.c
+++ C/exercises/External_variables.c
@@ -12,7 +12,30 @@ void copy(void);
 int main() 
 {
   /* longest input line specialized version */
-  
+  int length;
+
+  extern int max;
+  extern char longest_line[];
+  max = 0;
+
+  while ((length = getline()) > 0) {
+    if (length > max) {
+      max = length;
+      copy();
+    }
+  }
+
+  if (max > 0) {printf("%s", longest_line);}
   
   return 0;
+}
+
+/* --- FUNCTION DEFINITIONS --- */
+
+int getline(void){
+  
+}
+
+void copy(void){
+  
 }
