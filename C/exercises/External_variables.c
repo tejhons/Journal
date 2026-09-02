aa40cc91235a50623f58de8b5b24cdc17c8e1056
tejhons <tejh.tech@gmail.com>
2026-09-01 20:44:14 -0400

    EO1

--- C/exercises/External_variables.c
+++ C/exercises/External_variables.c
@@ -5,8 +5,9 @@ int max;
 char current_line[MAXLINE];
 char longest_line[MAXLINE];
 /* Notice initialized outside of main */
+/* States their type and causes storage to be allocated for them */
 
-int getline(void);
+int getline(void); /* the word void must be used for an explicitly empty list */
 void copy(void);
 
 int main() 
@@ -33,9 +34,25 @@ int main()
 /* --- FUNCTION DEFINITIONS --- */
 
 int getline(void){
+  int c, i;
+  extern char current_line[];
   
+  for (i=0; i<MAXLINE-1 && (c=getchar())!=EOF && c!='\n'; ++i) {
+    current_line[i] = c;
+  }
+
+  if (c == '\n') {
+    current_line[i] = c;
+    ++i;
+  }
+  current_line[i] = '\0';
+  return i;
 }
 
 void copy(void){
-  
+  int i;
+  extern char current_line[], longest_line[];
+
+  i = 0;
+  while ((longest_line[i] = current_line[i]) != '\0') {++i;}  
 }
